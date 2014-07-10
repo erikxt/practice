0d6f65f15cf1813d0e7f5272dc2662deaa710169
erik <erik@erik>
2014-07-11 02:22:16 +0800

    add sth

--- src/main/java/cache/MemcacheTest.java
+++ src/main/java/cache/MemcacheTest.java
@@ -1,13 +1,8 @@
 package cache;
 
-import net.spy.memcached.ConnectionFactoryBuilder;
 import net.spy.memcached.MemcachedClient;
-import net.spy.memcached.auth.AuthDescriptor;
-import net.spy.memcached.auth.PlainCallbackHandler;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 public class MemcacheTest {
@@ -18,7 +13,7 @@ public class MemcacheTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MemcacheTest spy = new MemcacheTest();
         try {
-			mc = new MemcachedClient(new InetSocketAddress("192.168.1.93", 11211));
+			mc = new MemcachedClient(new InetSocketAddress("192.168.1.100", 11211));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -26,38 +21,56 @@ public class MemcacheTest {
     }
 
     public void run() throws ExecutionException, InterruptedException {
+    	int s = 0, f = 0;
         for (int i = 0; i < 1000; i++) {
             String key = "key" + i;
             String value = "value" + i;
             if (mc.add(key, 0, value).get()) {
-                System.out.printf("added %s -> %s\n", key, value);
+//                System.out.printf("added %s -> %s\n", key, value);
+                s++;
             } else {
-                System.out.printf("ERROR ADDING %s -> %s\n", key, value);
+//                System.out.printf("ERROR ADDING %s -> %s\n", key, value);
+                f++;
             }
         }
+        System.out.println("s="+s+"   f="+f);
+        s = 0;
+        f = 0;
         for (int i = 0; i < 1000; i++) {
             String key = "key" + i;
             String value = "value" + (i * i);
             if (mc.set(key, 0, value).get()) {
-                System.out.printf("set %s -> %s\n", key, value);
+//                System.out.printf("set %s -> %s\n", key, value);
+            	s++;
             } else {
-                System.out.printf("ERROR SETTING %s -> %s\n", key, value);
+//                System.out.printf("ERROR SETTING %s -> %s\n", key, value);
+            	f++;
             }
         }
+        System.out.println("s="+s+"   f="+f);
+        s = 0;
+        f = 0;
         for (int i = 0; i < 1000; i++) {
             String key = "key" + i;
             String value = (String) mc.get(key);
-            System.out.printf("GET %s -> %s\n", key, value);
+//            System.out.printf("GET %s -> %s\n", key, value);
+            s++;
 
         }
+        System.out.println("s="+s+"   f="+f);
+        s = 0;
+        f = 0;
         for (int i = 0; i < 1000; i++) {
             String key = "key" + i;
             if (mc.delete(key).get()) {
-                System.out.printf("deleted %s\n", key);
+//                System.out.printf("deleted %s\n", key);
+            	s++;
             } else {
-                System.out.printf("ERROR DELETING %s\n", key);
+//                System.out.printf("ERROR DELETING %s\n", key);
+            	f++;
             }
         }
+        System.out.println("s="+s+"   f="+f);
         mc.shutdown();
     }
 
