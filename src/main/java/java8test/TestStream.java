e4998f5e14a8ea5735c3c2fd63d77f97462f743e
erik <iqboy118@gmail.com>
2015-02-05 01:18:20 +0800

    parallel stream

--- src/main/java/java8test/TestStream.java
+++ src/main/java/java8test/TestStream.java
@@ -3,6 +3,7 @@ package java8test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TestStream {
 	
@@ -45,5 +46,24 @@ public class TestStream {
 	            .map(person -> new Adult(person))
 	            .collect(Collectors.toCollection(ArrayList::new));
 		adultList.forEach(a->System.out.println(a.getAge()));
+		
+		long t0 = System.nanoTime();
+		 
+		//初始化一个范围100万整数流,求能被2整除的数字，toArray（）是终点方法
+		 
+		IntStream.range(0, 1_000_000).filter(p -> p % 2==0).toArray();
+		 
+		long t1 = System.nanoTime();
+		 
+		//和上面功能一样，这里是用并行流来计算
+		 
+		IntStream.range(0, 1_000_000).parallel().filter(p -> p % 2==0).toArray();
+		 
+		long t2 = System.nanoTime();
+		 
+		//我本机的结果是serial: 0.02s, parallel 0.09s???
+		//
+		 
+		System.out.printf("serial: %.2fs, parallel %.2fs%n", (t1 - t0) * 1e-9, (t2 - t1) * 1e-9);
 	}	
 }
